70706af965be2218148b64a0b4a53ea32f5b04af
Peter McCloskey <pcm17@pitt.edu>
2016-03-15 18:41:32 -0400

    Create CarTracker.java

---
+++ CarTracker.java
@@ -0,0 +1,12 @@
+public class CarTracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Car car = new Car();
+        IndexMinPQ<Double> pq = new IndexMinPQ<Double>(15);
+    }
+
+}
